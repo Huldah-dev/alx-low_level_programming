928f63e74fe47b202dcfcfefcc98dbdc36eabc97
Huldah Kaura <kaurahuldah406@gmail.com>
2022-03-22 12:31:03 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: pointer to a strng value
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + 1) != 0; i++);
+	for (i = i - 1; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,1 @@
-
-
-Write a function that prints a string, followed by a new line, to stdout.
+function that prints a string, in reverse, followed by a new line.
